88a64d4664d16bca399503bd89a2c193c3724296
websterxx <ismailakdiri3@gmail.com>
2021-12-31 19:04:40 +0100

    Update create ticket

--- app/Http/Controllers/CreateTicketController.php
+++ app/Http/Controllers/CreateTicketController.php
@@ -27,43 +27,50 @@ class CreateTicketController extends Controller
     {
         $user = User::find($ressource->user_id);
 
-        if ($request->anomalie == 1) {
-            Anomalie::create([
-                'name' => $request->description,
-            ]);
-
-            $anomalie = DB::table('anomalies')
-                ->select('anomalies.*')
-                ->where('name', '=', $request->description)
-                ->get()->first();
-
-            Ticket::create([
-                'user_id' => $user->id,
-                'anomalie_id' => $anomalie->id,
-                'ressource_id' => $ressource->id,
-                'description' => $request->description,
-            ]);
-
-            return redirect()->back();
-        } else {
-
-            $ticket = DB::table('tickets')
-                ->where('anomalie_id', '=', $request->anomalie)
-                ->where('ressource_id', '=', $ressource->id)
-                ->first();
-
-            if (is_null($ticket)) {
+        if ($request->description == null) {
+            $errors = ['Veuillez choisir un nom valide'];
+            return redirect()->back()->withErrors($errors);
+        }
+        else{
+            if ($request->anomalie == 1) {
+                Anomalie::create([
+                    'name' => $request->description,
+                ]);
+    
+                $anomalie = DB::table('anomalies')
+                    ->select('anomalies.*')
+                    ->where('name', '=', $request->description)
+                    ->get()->first();
+    
                 Ticket::create([
                     'user_id' => $user->id,
-                    'anomalie_id' => $request->anomalie,
+                    'anomalie_id' => $anomalie->id,
                     'ressource_id' => $ressource->id,
-                    'description' => 'null',
+                    'description' => $request->description,
                 ]);
+    
                 return redirect()->back();
             } else {
-                $errors = ['Cette anomalie est déja déclarer pour cette ressource'];
-                return redirect()->back()->withErrors($errors);
+    
+                $ticket = DB::table('tickets')
+                    ->where('anomalie_id', '=', $request->anomalie)
+                    ->where('ressource_id', '=', $ressource->id)
+                    ->first();
+    
+                if (is_null($ticket)) {
+                    Ticket::create([
+                        'user_id' => $user->id,
+                        'anomalie_id' => $request->anomalie,
+                        'ressource_id' => $ressource->id,
+                        'description' => 'null',
+                    ]);
+                    return redirect()->back();
+                } else {
+                    $errors = ['Cette anomalie est déja déclarer pour cette ressource'];
+                    return redirect()->back()->withErrors($errors);
+                }
             }
         }
+        
     }
 }
