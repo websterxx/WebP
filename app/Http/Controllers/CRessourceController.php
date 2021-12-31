0f0f6a78dfd33038afefa941bde1fc3fca7a2b75
oussama <oussamajallali6@gmail.com>
2021-12-31 16:44:21 +0100

    Update msg

--- app/Http/Controllers/CRessourceController.php
+++ app/Http/Controllers/CRessourceController.php
@@ -29,26 +29,24 @@ class CRessourceController extends Controller
             'localisation' => 'required|max:255',
         ]);
 
-        
+
         $ressource = DB::table('ressources')
-        ->where('name', '=', $request->name)
-        ->where('localisation', '=', $request->localisation)
-        ->first();
+            ->where('name', '=', $request->name)
+            ->where('localisation', '=', $request->localisation)
+            ->first();
 
         if (is_null($ressource)) {
-        $id = DB::select("SHOW TABLE STATUS LIKE 'ressources'");
-        $next_id = $id[0]->Auto_increment;
+            $id = DB::select("SHOW TABLE STATUS LIKE 'ressources'");
+            $next_id = $id[0]->Auto_increment;
 
-        $request->user()->ressources()->create([
-            'name' => $request->name,
-            'localisation' => $request->localisation,
-            'url' => 'http://192.168.76.76/public/createticket/' . ($next_id),
-        ]);
-        return back();
-        }
-
-        else{
-            $errors = ['Ressource de même nom existe dans la même localisation : Veuillez changez le nom '];
+            $request->user()->ressources()->create([
+                'name' => $request->name,
+                'localisation' => $request->localisation,
+                'url' => 'http://192.168.76.76/public/createticket/' . ($next_id),
+            ]);
+            return back();
+        } else {
+            $errors = ['Ressource de même nom existe dans cette localisation'];
             return redirect()->back()->withErrors($errors)->withInput($request->input());
         }
     }
